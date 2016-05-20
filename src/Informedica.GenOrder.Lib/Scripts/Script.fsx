43a4d051b7f20136bf759efb735f46af07f5dc77
Casper Bollen <halcwb@gmail.com>
2016-05-20 13:57:55 +0200

    Updated GenUnits

--- src/Informedica.GenOrder.Lib/Scripts/Script.fsx
+++ src/Informedica.GenOrder.Lib/Scripts/Script.fsx
@@ -18,7 +18,7 @@ module SV = Solver
 module UN = Unit
 module UG = Informedica.GenUnits.Lib.UnitGroup
 
-"mg(Mass)/kg(Weight)/min(Time)" |> Informedica.GenUnits.Lib.CombiUnit.fromString
+"mg[Mass]/kg[Weight]/min[Time]" |> Informedica.GenUnits.Lib.CombiUnit.fromString
 |> UG.fromUnit
 |> UG.getUnits
 |> List.map Informedica.GenUnits.Lib.CombiUnit.toString
@@ -34,32 +34,32 @@ let print ord =
         printfn "%s" s
     ord
         
-for o in OR.createNew [["Genta"]] "mg(Mass)" "ml(Volume)" "kg(Weight)" |> OR.toString do
+for o in OR.createNew [["Genta"]] "mg[Mass]" "ml[Volume]" "kg[Weight]" |> OR.toString do
     printfn "%s" o
 
-for o in OR.createNew [["dopamine"];["sodium";"chloride"]] "mg(Mass)" "ml(Volume)" "kg(Weight)" |> OR.toString do
+for o in OR.createNew [["dopamine"];["sodium";"chloride"]] "mg[Mass]" "ml[Volume]" "kg[Weight]" |> OR.toString do
     printfn "%s" o
 
-let pcm = OR.createNew [["paracetamol"]] "mg(Mass)" "tabl(Shape)" "kg(Weight)" 
+let pcm = OR.createNew [["paracetamol"]] "mg[Mass]" "tabl[Shape]" "kg[Weight]" 
 let pre = PR.discontinuous
 
-let ord = OD.createNew "kg(Mass)" pcm pre "oral"
+let ord = OD.createNew "kg[Mass]" pcm pre "oral"
 
 
 ord |> print |> ignore
 ord
-|> OD.solve "paracetamol" MP.ItemComponentQty SV.Vals [240N; 300N; 500N] (UN.qtyUnit "mg(Mass)")
+|> OD.solve "paracetamol" MP.ItemComponentQty SV.Vals [240N; 300N; 500N] (UN.qtyUnit "mg[Mass]")
 |> print
 |> OD.solve "" MP.Freq SV.Vals [2N;3N;4N;5N;6N] (UN.freqUnit)
 |> print
-|> OD.solve "paracetamol" MP.OrderableOrderableQty SV.Vals [1N] (UN.qtyUnit "tabl(Tablet)")
+|> OD.solve "paracetamol" MP.OrderableOrderableQty SV.Vals [1N] (UN.qtyUnit "tabl[Tablet]")
 |> print
-|> OD.solve "paracetamol" MP.OrderableDoseQty SV.Vals [1N] (UN.qtyUnit "tabl(Tablet)")
+|> OD.solve "paracetamol" MP.OrderableDoseQty SV.Vals [1N] (UN.qtyUnit "tabl[Tablet]")
 |> print
-|> OD.solve "paracetamol" MP.ItemDoseTotal SV.MaxIncl [4N] (UN.qtyUnit "gram(Mass)")
+|> OD.solve "paracetamol" MP.ItemDoseTotal SV.MaxIncl [4N] (UN.qtyUnit "gram[Mass]")
 |> print
-|> OD.solve "paracetamol" MP.ItemDoseAdjustTotalAdjust SV.MaxIncl [90N] ("mg(Mass)" |> UN.totalUnit |> UN.adjUnit "kg(Weight)")
+|> OD.solve "paracetamol" MP.ItemDoseAdjustTotalAdjust SV.MaxIncl [90N] ("mg[Mass]" |> UN.totalUnit |> UN.adjUnit "kg[Weight]")
 |> print
-|> OD.solve "" MP.AdjustQty SV.Vals [10N] (UN.qtyUnit "kg(Weight)")
+|> OD.solve "" MP.AdjustQty SV.Vals [10N] (UN.qtyUnit "kg[Weight]")
 |> print
 |> ignore
--- src/Informedica.GenOrder.Lib/Unit.fs
+++ src/Informedica.GenOrder.Lib/Unit.fs
@@ -10,15 +10,15 @@ module Unit =
     module UN = Informedica.GenUnits.Lib.CombiUnit
 
     /// Create a frequency unit
-    let freqUnit = "1 X(Count)/day(Time)" |> UN.fromString
+    let freqUnit = "1 X[Count]/day[Time]" |> UN.fromString
     /// Create a time unit
-    let timeUnit = "1 hour(Time)"  |> UN.fromString
+    let timeUnit = "1 hour[Time]"  |> UN.fromString
     /// Create a quantity unit
     let qtyUnit s = s |> UN.fromString
     /// Create a total unit
-    let totalUnit s = s + "/day(Time)"  |> UN.fromString
+    let totalUnit s = s + "/day[Time]"  |> UN.fromString
     /// Create a rate unit
-    let rateUnit s  = s + "/hour(Time)" |> UN.fromString
+    let rateUnit s  = s + "/hour[Time]" |> UN.fromString
     /// Create a unit `s1` per unit `s2`
     let perUnit s2 s1 = "1 " + s1 + "/1 " + s2 |> UN.fromString
     /// Create a unit `cu` adjusted by unit `s2`
